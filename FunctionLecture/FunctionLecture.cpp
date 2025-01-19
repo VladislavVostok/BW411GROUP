7171407d3b666b2558998419d61e7dba3986d1ef
Valadislav East <valadislav.sage@yandex.ru>
2025-01-19 21:30:37 +0300

    Функции

---
+++ FunctionLecture/FunctionLecture.cpp
@@ -0,0 +1,117 @@
+﻿// FunctionLecture.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+
+void ShowArray(int arr[], int size);
+void InitArray(int arr[], int size);
+int summa(int array[][5], int size_row, int size_col);
+void InitMatrix(int matr[][3], int rows, int cols);
+void ShowMatrix(int matr[][3], int rows, int cols);
+
+int A = 10;
+
+int main()
+{
+
+    int A = 66;
+    cout << ::A << endl;
+
+
+
+    srand(time(nullptr));
+
+    
+
+    const int aSize = 10;
+    int iArr[aSize];
+
+    InitArray(iArr, aSize);
+    cout << A << endl;
+
+    ShowArray(iArr, aSize);
+    cout << A << endl;
+
+    const int mRows = 3;
+    const int mCols = 3;
+    int iMatr[mRows][mCols];
+    InitMatrix(iMatr, mRows, mCols);
+
+
+    ShowMatrix(iMatr, mRows, mCols);
+
+
+    //int temp = 4;
+
+    //if(temp == 4) {
+    //    int b = 5;
+    //    cout << b;
+    //}
+
+
+
+    return 0;
+}
+
+
+// отображение массива
+void ShowArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+/* инициализируем массив внутри функции случайными
+числами в отличии от обычных переменных изменения
+элементов массива, происходящие внутри функции
+при выходе из функции сохраняются
+*/
+void InitArray(int arr[], int size) {
+    
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+    }
+}
+
+
+
+int summa(int array[][5], int size_row, int size_col) {
+    int res = 0;
+    for (int i = 0; i < size_row; i++)
+        for (int j = 0; j < size_col; j++)
+            res += array[i][j];
+    return res;
+}
+
+// отображение матрицы
+void ShowMatrix(int matr[][3], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void InitMatrix(int matr[][3], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matr[i][j] = rand() % 10;
+        }
+    }
+    cout << endl;
+}
+
+
+
+
--- FunctionPractice/FunctionPractice.cpp
+++ FunctionPractice/FunctionPractice.cpp
@@ -1,4 +1,7 @@
 ﻿#include <iostream>
+#define CONSTANNTA 54.34235;
+
+#pragma region MyRegion
 
 // Задание 1. Написать функцию, выводящую на экран 
 // прямоугольник с высотой N и шириной K.
@@ -11,6 +14,7 @@ void drawRectangle(char sign, int height, int weight) {
     }
 }
 
+#pragma endregion
 // Задание 2. Написать функцию, вычисляющую факториал переданного ей числа.
 
 unsigned long long calcFactorial(int n) {
@@ -35,13 +39,69 @@ bool is_prime(int num) {
     }
 
     return true;
-
 }
+    // Задание 4. Написать функцию, которая возвращает куб числа.
+
+
+    // Задание 5. Написать функцию для нахождения наибольшего из двух чисел.
+    int getMax(int a, int b) {
+        if (a > b) 
+            return a;
+
+        return b;
+    }
+
+
+    float getMax(float a, float b) {
+        if (a > b)
+            return a;
+
+        return b;
+    }
+
+
+    double getMax(double a, double b) {
+        if (a > b)
+            return a;
+
+        return b;
+    }
+
+    double getMax(double a, double b, double c) {
+        if (a > b && a > c)
+            return a;
+        else if (b > c && b > a)
+            return b;
+
+        return c;
+    }    
+    
+    double getMax(double a=1.0, double b=1.0, double c=1.0, double d = 1.0) {
+        if (a > b && a > c)
+            return a;
+        else if (b > c && b > a)
+            return b;
+
+        return c;
+    }
+
+    // Задание 6. Написать функцию, которая возвращает истину, 
+    // если передаваемое значение положительное и ложь, если отрицательное.
+    
+    bool isPositive(int a = -15) {
+        if (a >= 0)
+            return true;
+
+        return false;
+    }
 
 
 int main()
 {
     setlocale(LC_ALL, "");
+
+    std::cout << CONSTANNTA;
+
     // Задание 1.
     int N;
     int K;
@@ -65,5 +125,26 @@ int main()
     if (is_prime(2) == true) {
         std::cout << "Все четко";
     }
+
+
+    // Задание 5.
+    std::cout << getMax(1, 1000) << std::endl;
+    std::cout << getMax(3.4f, 6.2f) << std::endl;
+    std::cout << getMax(54.2, 234.7) << std::endl;
+    std::cout << getMax(54.2, 234.7, 345.1, 34.2) << std::endl;
+
+
+
+    // Задание 6.
+
+    int x = -40;
+    if (isPositive()) {
+        std::cout << "Положительное!";
+    }
+    else {
+        std::cout << "Отрицательное!";
+    }
+
+    std::cout << isPositive(-40) << std::endl;
 
 }
