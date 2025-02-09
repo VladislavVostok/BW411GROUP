3e11b16c72dcf43271ad95fd92ffe46c1344d7c3
Valadislav East <valadislav.sage@yandex.ru>
2025-02-09 21:39:14 +0300

    09,02,2025

---
+++ Lesson_15_DoubleArrayDinamycMemmory/Lesson_15_DoubleArrayDinamycMemmory.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    int** A = new int* [5];
+
+    for (int i = 0; i < 5; i++) {
+
+        A[i] = new int[5];
+
+        for (int j = 0; i < 5; j++) {
+            A[i][j] = 6;
+        }
+
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        delete[]A[i];
+    }
+    delete[]A;
+
+
+}
