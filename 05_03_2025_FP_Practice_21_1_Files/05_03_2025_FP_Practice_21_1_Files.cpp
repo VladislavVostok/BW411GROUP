41d8d14dc76cc3db5d699cdddf2353d02540d6f7
Vladislav East <vladislav.sage@yandex.ru>
2025-03-05 21:17:29 +0300

    Files Streams

---
+++ 05_03_2025_FP_Practice_21_1_Files/05_03_2025_FP_Practice_21_1_Files.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+#define MAX_WORD 100
+
+bool isLongWord(const char* word) {
+    return strlen(word) >= 7;
+}
+
+bool isLongWord(const string& word) {
+    return word.length() >= 7;
+}
+
+void task1(const char* inputFile, const char* outputFile) {
+    FILE* in, * out;
+    
+    if (fopen_s(&in, inputFile, "r") != 0 || fopen_s(&out, outputFile, "w") != 0) {
+        perror("Ошибка открытия файла");
+        return;
+    }
+
+    char word[MAX_WORD];
+
+    while (!feof(in)) {
+        fgets(word, MAX_WORD - 1, in);
+        if (isLongWord(word)) {
+            fprintf_s(out, "%s", word);
+        }
+    }
+
+    fclose(in);
+    fclose(out);
+}
+
+void task1WithStream(const string& inputFile, const string& outputFile) {
+    ifstream inFile(inputFile);
+    ofstream outFile(outputFile, ios::app);
+
+    if (!inFile || !outFile) {
+        cerr << "Ошибка открытия файла!" << endl;
+        return;
+    }
+
+    string word;
+
+    while (getline(inFile, word)) {
+        if (isLongWord(word)) {
+            outFile << word << endl;
+        }
+    }
+
+    outFile.close();
+    inFile.close();
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    //task1("input.txt", "output1.txt");
+    task1WithStream("input.txt", "output1.txt");
+}
+
---
+++ 05_03_2025_Lectures_Files_Streams/05_03_2025_Lectures_Files_Streams.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+    //ofstream out;
+    //out.open("hello1.txt");
+    //out.close();
+
+    //out.open("hello1.txt", ios::app);
+    //out.close();
+
+    //ifstream in;
+    //in.open("hello1.txt");
+    //in.close();
+
+    //fstream fs;
+    //fs.open("hello1.txt");
+    //fs.close();
+
+    //fstream out1("hello.txt");
+    //out1.close();
+
+
+
+}
