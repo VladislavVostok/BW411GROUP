afe9319c1e23bd7b5b28353f8f129c396cff646c
Valadislav East <valadislav.sage@yandex.ru>
2025-05-17 21:25:47 +0300

    exam

---
+++ 17_05_2025_Exam/17_05_2025_Exam_1_zadanie.cpp
@@ -0,0 +1,91 @@
+﻿//#include <iostream>
+//#include <string>
+//#include <random>
+//#include <ctime>
+//#include <vector>
+//
+//using namespace std;
+//
+//class PassGenerator {
+//private:
+//    string rusSymbols = "йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ";
+//    string latSymbols = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+//    string specSymbols = "~!@#$%^&*()<>{}[]|;:?.,";
+//    string digSymbols = "0123456789";
+//
+//    mt19937 generator;
+//
+//public:
+//    PassGenerator() {
+//        generator.seed(time(nullptr));
+//    }
+//
+//    string gen_pass(int lendth, bool dig, bool let, bool spec, bool rusLet) {
+//
+//        string characters;
+//        string pass;
+//
+//        if (dig) {
+//            characters += digSymbols;
+//        }
+//
+//        if (let) {
+//            characters += latSymbols;
+//        }
+//        if (rusLet) {
+//            characters += rusSymbols;
+//        }
+//
+//        if (spec) {
+//            characters += specSymbols;
+//        }
+//
+//        if (characters.empty()) {
+//            cout << "Нет выбора символов для построения пароля!";
+//            return "";
+//        }
+//
+//        uniform_int_distribution<> distibutions(0, characters.size() - 1);
+//
+//        for (int i = 0; i < lendth; ++i) {
+//            pass += characters[distibutions(generator)];
+//        }
+//
+//        return pass;
+//
+//    }
+//
+//    string genStrong() {
+//        return gen_pass(12, true, true, true, true);
+//    }    
+//    
+//    string genMedium() {
+//        return gen_pass(12, false, true, true, false);
+//    }    
+//    
+//    string genEasy() {
+//        return gen_pass(12, false, false, false, true);
+//    }
+//
+//};
+//
+//
+//int main()
+//{
+//    //system("chcp 1251>null");
+//    setlocale(LC_ALL, "");
+//
+//    PassGenerator generator;
+//
+//
+//    //cout << gen_pass(15, true, true, true, false) << endl;
+//    //cout << gen_pass(20, true, true, false, false) << endl;
+//    //cout << gen_pass(50, true, false, false, false) << endl;
+//    cout << generator.genEasy() << endl;
+//    cout << generator.genMedium() << endl;
+//    cout << generator.genStrong() << endl;
+//    cout << generator.gen_pass(30, true, true, true, true) << endl;
+//}
+//
+//
+//
