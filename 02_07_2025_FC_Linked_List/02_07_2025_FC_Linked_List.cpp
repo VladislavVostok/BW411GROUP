70d9f736752e7823fc51ebee9ee093849a8df09f
Vladislav East <vladislav.sage@yandex.ru>
2025-07-06 17:51:31 +0300

    LinkedList

--- 02_07_2025_FC_Linked_List/02_07_2025_FC_Linked_List.cpp
+++ 02_07_2025_FC_Linked_List/02_07_2025_FC_Linked_List.cpp
@@ -1,6 +1,32 @@
 ﻿#include <iostream>
+#include "LinkedList.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    LinkedList<int> list;
+
+    list.AddToTail(10);
+    list.AddToTail(20);
+    list.AddToTail(30);
+    list.Show();
+
+
+    list.InsertAt(1, 15);
+    list.Show();
+
+    list.DeleteAt(2);
+    list.Show();
+
+    int pos = list.Find(15);
+    cout << "Элемент 15 находится по индексу [" << pos << "];" << endl;
+
+    int replacements = list.Replace(10, 100);
+
+    cout << "Замене подверглось " << replacements << " объектов;" << endl;
+
+    list.Reverse();
+    list.Show();
+
+    return 0;
 }
